d02188c36cb74a5e6bbcf97b0945dd8645f814ce
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-02-03 19:45:09 +0100

    Init projektu

---
+++ app/src/main/java/com/zywczas/weather/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.zywczas.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.zywczas.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    Text("Hello World ;)")
+                }
+            }
+        }
+    }
+}
