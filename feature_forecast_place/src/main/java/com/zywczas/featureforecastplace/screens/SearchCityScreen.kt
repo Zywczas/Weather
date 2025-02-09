ceb61700d87fce5a6500d77dc3b7118be9164f8d
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-02-09 20:53:45 +0100

    Added search bar for cities

--- feature_forecast_place/src/main/java/com/zywczas/featureforecastplace/screens/SearchCityScreen.kt
+++ feature_forecast_place/src/main/java/com/zywczas/featureforecastplace/screens/SearchCityScreen.kt
@@ -1,16 +1,23 @@
 package com.zywczas.featureforecastplace.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.zywczas.commoncompose.components.CityListItem
+import com.zywczas.commoncompose.components.OutlinedTextInput
 import com.zywczas.commoncompose.components.Toolbar
 import com.zywczas.commoncompose.theme.PreviewTheme
+import com.zywczas.commoncompose.theme.Spacing
 import com.zywczas.commonutil.R
+import com.zywczas.commonutil.RegexExps
 import com.zywczas.featureforecastplace.viewmodel.City
 import com.zywczas.featureforecastplace.viewmodel.SearchCityViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -26,6 +33,8 @@ fun SearchCityScreen(
     SearchCityScreen(
         cities = viewModel.cities,
         onCityClick = onCityClick,
+        searchText = viewModel.searchText,
+        onSearchTextChanged = viewModel::onSearchTextChanged
     )
 }
 
@@ -33,11 +42,24 @@ fun SearchCityScreen(
 private fun SearchCityScreen(
     cities: List<City>,
     onCityClick: (PlaceForecastArgs) -> Unit,
+    searchText: String,
+    onSearchTextChanged: (String) -> Unit
 ) {
     Column {
         Toolbar(stringResource(R.string.search_city_screen))
+
+        OutlinedTextInput(
+            value = searchText,
+            onValueChange = onSearchTextChanged,
+            modifier = Modifier.padding(horizontal = Spacing.m),
+            regexFilter = RegexExps.INPUT_CITY_TYPING
+        )
+
+        Spacer(Modifier.height(Spacing.s))
+
         LazyColumn {
             items(cities) { city ->
+
                 CityListItem(
                     city.name,
                     onClick = { onCityClick(PlaceForecastArgs(lat = city.lat, lon = city.lon, placeName = city.name)) }
@@ -63,6 +85,8 @@ private fun PreviewSearchCityScreen() {
                 City(name = "Karpacz"),
             ),
             onCityClick = {},
+            searchText = "Warszawa",
+            onSearchTextChanged = {}
         )
     }
 }
--- feature_forecast_place/src/main/java/com/zywczas/featureforecastplace/viewmodel/SearchCityViewModel.kt
+++ feature_forecast_place/src/main/java/com/zywczas/featureforecastplace/viewmodel/SearchCityViewModel.kt
@@ -7,10 +7,17 @@ import com.zywczas.commonutil.BaseViewModel
 
 internal class SearchCityViewModel : BaseViewModel() {
 
+    var searchText by mutableStateOf("")
+        private set
+
     var cities by mutableStateOf<List<City>>(emptyList())
         private set
 
     fun init() {
         cities = listOf(City(name = "Bydgoszcz", lat = 52.2297, lon = 21.0122))
+    }
+
+    fun onSearchTextChanged(text: String) {
+        searchText = text
     }
 }
