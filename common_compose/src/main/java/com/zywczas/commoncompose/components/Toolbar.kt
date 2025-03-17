45b58bef9e9d4853655689aad353c5e2136145b2
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-03-17 19:38:34 +0100

    Added navigation back to weather details screen. Changed primary color.

--- common_compose/src/main/java/com/zywczas/commoncompose/components/Toolbar.kt
+++ common_compose/src/main/java/com/zywczas/commoncompose/components/Toolbar.kt
@@ -1,17 +1,40 @@
 package com.zywczas.commoncompose.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.zywczas.commoncompose.theme.PreviewTheme
+import com.zywczas.commonutil.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Toolbar(title: String) {
+fun Toolbar(
+    title: String,
+    onBackClick: (() -> Unit)? = null,
+) {
     TopAppBar(
         title = { Text(title) },
+        navigationIcon = onBackClick?.let {
+            {
+                IconButton(
+                    onClick = onBackClick,
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.content_description_navigate_back),
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        } ?: {},
     )
 }
 
@@ -19,6 +42,9 @@ fun Toolbar(title: String) {
 @Composable
 private fun PreviewToolbar() {
     PreviewTheme {
-        Toolbar("PreviewScreen")
+        Toolbar(
+            title = "PreviewScreen",
+            onBackClick = {},
+        )
     }
 }
