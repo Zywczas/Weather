421b8738ffbe8ca09b386a8b6594ec810931e75f
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-03-19 14:48:45 +0100

    test

--- buildSrc/src/main/kotlin/com/zywczas/buildutils/SystemEnvs.kt
+++ buildSrc/src/main/kotlin/com/zywczas/buildutils/SystemEnvs.kt
@@ -1,5 +1,5 @@
 package com.zywczas.buildutils
 
 import org.gradle.api.Project
-
+//
 fun getEnvElseLocal(key: String, project: Project): String = System.getenv(key) ?: project.property(key) as String
