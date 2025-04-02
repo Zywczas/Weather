9ef292de74eb0e62dd61e72dd7b7d92f317d0430
piotr.zywczewski <piotr.zywczewski@gmail.com>
2025-04-02 20:47:36 +0200

    Implemented kotlinx date time.

--- common_utils/src/commonMain/kotlin/com/zywczas/commonutils/extensions/Date.kt
+++ common_utils/src/commonMain/kotlin/com/zywczas/commonutils/extensions/Date.kt
@@ -4,4 +4,4 @@ package com.zywczas.commonutils.extensions
  * @return just hour number, e.g. '14'
  */
 //val Date.hour: String get() = SimpleDateFormat("HH", Locale.getDefault()).format(this)
-//todo update to KMM use kotlin date
+//todo update to KMM, probably not needed of Long.toDate works with kotlinx date
--- common_utils/src/commonMain/kotlin/com/zywczas/commonutils/extensions/Long.kt
+++ common_utils/src/commonMain/kotlin/com/zywczas/commonutils/extensions/Long.kt
@@ -1,6 +1,12 @@
 package com.zywczas.commonutils.extensions
 
-//todo update to KMM use kotlin date
-//fun Long.unixDateTimeToDate(): Date = Date(this * UNIX_DATE_TIME_MULTIPLIER_TO_JAVA_DATE)
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
-private const val UNIX_DATE_TIME_MULTIPLIER_TO_JAVA_DATE = 1000
+//todo update to KMM, check outcome
+//fun Long.unixDateTimeToDate(): Date = Date(this * UNIX_DATE_TIME_MULTIPLIER_TO_JAVA_DATE)
+//private const val UNIX_DATE_TIME_MULTIPLIER_TO_JAVA_DATE = 1000
+
+fun Long.unixDateTimeToDate(): LocalDateTime = Instant.fromEpochSeconds(this).toLocalDateTime(TimeZone.currentSystemDefault())
