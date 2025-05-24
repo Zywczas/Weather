ca17072037a0158c6ebb209746a2f25938b90998
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-05-24 18:12:25 +0200

    poprawilem importy z serialization

--- network_caller/build.gradle.kts
+++ network_caller/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.kotlin.compose.compiler)
-    alias(libs.plugins.kotlin.serialization)
+//    alias(libs.plugins.kotlin.serialization)
     // alias(libs.plugins.detekt)todo update to KMM
 }
 
