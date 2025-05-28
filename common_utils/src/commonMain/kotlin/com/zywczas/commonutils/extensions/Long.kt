ba613cdb732820bf6ef5cc084d9f53186677cc07
Piotr Zywczewski <piotr.zywczewski@gmail.com>
2025-05-28 16:28:52 +0200

    Removed unnecessary TODOs

--- common_utils/src/commonMain/kotlin/com/zywczas/commonutils/extensions/Long.kt
+++ common_utils/src/commonMain/kotlin/com/zywczas/commonutils/extensions/Long.kt
@@ -5,8 +5,4 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
-//todo update to KMM, check outcome
-//fun Long.unixDateTimeToDate(): Date = Date(this * UNIX_DATE_TIME_MULTIPLIER_TO_JAVA_DATE)
-//private const val UNIX_DATE_TIME_MULTIPLIER_TO_JAVA_DATE = 1000
-
 fun Long.unixDateTimeToDate(): LocalDateTime = Instant.fromEpochSeconds(this).toLocalDateTime(TimeZone.currentSystemDefault())
